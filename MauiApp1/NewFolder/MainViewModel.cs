67506fe682b447779c9fb97179612786b32f16a1
cedro <cedro@DESKTOP-051QEFJ>
2022-07-13 07:48:43 -0400

    Add project files.

---
+++ MauiApp1/NewFolder/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace MauiApp1.NewFolder
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string text;
+        public string Text
+        {
+            get => text;
+            set
+            {
+                text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
